1a9948a7bab846edfc4bbb0bb783fb522feb118a
mattjneum5 <106887982+mattjneum5@users.noreply.github.com>
2022-06-10 13:23:24 -0400

    Add files via upload

---
+++ number guesser.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1,x)
+
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a random number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry. Guess again. Too low.')
+        elif guess > random_number:
+            print('Sorry. Guess again. Too high.')
+    print(f'Yay! You have guessed the number {random_number} correctly!')
+
+
